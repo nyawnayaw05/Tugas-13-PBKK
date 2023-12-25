cfe4e07f36fc192ebc15bc4584f1726d51fdf21a
nyawnayaw <hglenaya@gmail.com>
2023-12-25 15:55:47 +0700

    update

---
+++ script.js
@@ -0,0 +1,16 @@
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycby8uBC4dO_TXH8mkazf5iAXIWKS1322Z1zEiJcWHLPqEJOw2ckfzA2-xB5DmOgMr-3q2w/exec";
+
+const form = document.forms["contact-form"];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) =>
+      alert("Thank you! your form is submitted successfully.")
+    )
+    .then(() => {
+      window.location.reload();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
